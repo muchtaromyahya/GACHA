f570d46a436615d13690981d2d4762e912bcd024
muchtaromyahya <muchtaromyahya@gmail.com>
2019-04-06 20:18:01 +0700

    pindah

--- GACHA/application/views/header.php
+++ GACHA/application/views/header.php
@@ -4,7 +4,6 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">  
   <title>GACHA</title>
-  <link rel="stylesheet" type="text/css" href="home.css">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
@@ -44,12 +43,6 @@
     .h1{
       font-size: 90px;
       color: cornsilk;
-    }
-    body{
-      background-image: url('assets/background.png');
-      position: relative;
-      background-size: cover;
-      
     }
     .nav-link .color{
         color: antiquewhite;
@@ -92,21 +85,5 @@
       </nav>
       
   <!-- Masthead -->
-  <header class="">
-    <div class="container">
-      <div class="row  align-items-center justify-content-center text-center">
-        <div class="col-lg-10 align-self-end">
-          <h1 class="text-uppercase text-white font-weight-bold"></h1>
-          <hr class="divider my-4">
-        </div>
-        <div class="col-lg-8 align-self-baseline">
-          <p class="text-white-75 font-weight-light mb-5">sus</p><br><br>
-          <p class="h1">Bingung Mencari Tempat magang yang cocok?</p><br>
-          <p class="text-white-75 font-weight-light mb-5">Start Bootstrap can help you build better websites using the Bootstrap framework! Just download a theme and start customizing, no strings attached!</p>
-          <a class="btn btn-primary btn-xxl js-scroll-trigger" href="#about">Cek Lowongan</a>
-        </div>  
-      </div>
-    </div>
-  </header>
 </body>
 </html>
