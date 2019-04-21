1f2aac2b70e9d4f8e522662b21ea324d815d28c7
geryn25 <35487884+geryn25@users.noreply.github.com>
2019-04-21 23:45:00 +0700

    Update header1.php

--- application/views/header1.php
+++ application/views/header1.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>GACHA</title>
+    <link href = "<?php echo base_url('assets/logo_gacha2.png') ?>" rel="icon" type="image/png">
+
+
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
@@ -136,7 +139,7 @@ input[type=text-field],  [type=date]{
 
 </head>
 <body >
-    <nav class = "navbar-nav navbar-expand-lg navbar-light bg-light sticky-top" style="padding:10px;">
+    <nav class = "navbar-nav navbar-expand-lg navbar-dark bg-dark sticky-top" style="padding:10px;">
 
         <button class ="navbar-toggler" data-toggle="collapse" data-target="#collapse_target">
             <span class = "navbar-toggler-icon"></span>
@@ -144,8 +147,8 @@ input[type=text-field],  [type=date]{
 
         <div class = "collapse navbar-collapse" id = "collapse_target">
 
-        <a class = "navbar-brand"><img src = "<?php echo base_url('application/views/assets/placeholder.jpg'); ?>" style = "width: 40px;height: 20%;"></a>
-        <span class = "navbar-text" style = "padding-bottom:10px;margin-right: 580px;color: black;font-size: 25x;font-weight: bold;">GACHA</span>
+        <a class = "navbar-brand" href="<?php echo base_url('Home'); ?>"><img src = "<?php echo base_url('assets/logo_gacha.png') ?>" style = "width: 100px"></a>
+        <span class = "navbar-text" style = "padding-bottom:10px;margin-right: 580px;color: black;font-size: 25x;font-weight: bold;"></span>
 
             <ul class = "navbar-nav mr-auto">
                 <li class = "nav-item">
