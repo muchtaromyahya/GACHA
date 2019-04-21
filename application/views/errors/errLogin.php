59187480a0067698e8ee14e89919c1084e8d415d
geryn25 <35487884+geryn25@users.noreply.github.com>
2019-04-21 11:16:25 +0700

    c

--- application/views/errors/errLogin.php
+++ application/views/errors/errLogin.php
@@ -1,6 +1,6 @@
     
 <div class='alert alert-danger alert-dismissible fade show' role='alert'>
-<strong><?= $_SESSION['alert'] ?></strong>
+<strong><?= $_SESSION['alert'] ?>></strong>
     <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
         <span aria-hidden='true'>&times;</span>
     </button>
