0f5f8d18f04238d98e9a29e1928c16aeb67aa5be
gestaporayhan <rayhan_alifinzi@hotmail.com>
2019-04-23 02:05:59 +0700

    Update About.php

---
+++ application/views/About.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>GACHA</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>  
+    </head>
+    <style>
+        .container{
+            width:80%;
+            margin-left:20%;
+            font-family: Roboto;
+            background-color:skyblue;
+        }
+        h2{
+            font-size: 22px;
+            color:royalblue
+            text-align: center;
+            margin-top: 5%;
+            border: 2px;
+            text-align:center;
+        }
+        img{
+            width: 20%;
+            text-align: center;
+            margin-left: 40%;
+        }
+        .contact{
+            display: flex;
+            width:100%;
+        }
+        .row .footer{
+            margin-bottom:0px;
+            bottom: 0px;
+        }
+        .row .a1{
+            width: 30%;
+            background-color:skyblue;
+        }
+       
+        
+
+        
+    </style>
+    <body>
+        <div class = "continer">
+            <div class = "row" style = "margin-top: 150px;margin-left: 50px;margin-right: auto;">
+                <div class = "col-md">
+                    <h2 > Website GACHA merupakan sebuah website yang menyediakan lowongan magang kepada mahasiswa biasa yang sedang berkuliah maupun masyarakat yang mencari lowongan kerja praktik maupun magang . Jadi dengan adanya website ini, mahasiswa akan mudah mencari perusahaan perusahaan yang menyediakan lowongan magang</h2>
+                </div>
+                <div class = "col-md">
+                        <img src = "<?php echo base_url('application\views\assets\logo_gacha.png'); ?>" style="cursor:pointer;" href='#'>
+                    </div>
+            </div>
+
+            <div class = "row" style = "margin-top: 250px;margin-left: 50px;margin-right: auto;">
+                <div class = "col-md">
+                        <img src = "<?php echo base_url('application\views\assets\logo_gacha.png'); ?>" style="cursor:pointer;" href='#'>
+                </div>
+                <div class = "col-md">
+                    <h2> Dengan menggunakan GACHA, mahasiswa tidak perlu mencari lagi lowongan magang dengan susah payah, GACHA memberikan informasi yang detail serta lengkap dalam hal dunia magang, baik dari segi informasi lowongan yang ada maupun perusahaan yang menjadi mitra GACHA</h2>
+                </div>
+            </div>
+
+            <div class = "row" style = "margin-top: 250px;margin-left: 50px;margin-right: auto;">
+                    <div class = "col-md">
+                            <h2> Mitra perusahaan dapat dengan mudah mendapatkan serta menyaring para aplikan magang sesuai dengan kebutuhan dan latar belakang aplikan</h2>
+                    </div>
+                    <div class = "col-md">
+                            <img src = "<?php echo base_url('application\views\assets\logo_gacha.png'); ?>" style="cursor:pointer;" href='#'>
+                        </div>
+            </div>
+
+    <footer class="page-footer font-small indigo" style = "background-color:grey;opacity:0.8;margin-top:200px;">
+
+
+      <div class="row d-flex text-center justify-content-center mb-md-0 mb-4">
+
+        <div class="col-md-8 col-12 mt-5" style = "font-size:20px;">
+          <p  style="line-height: 1.7rem;font-size:20px;"> Alamat</p>
+          <p style="line-height: 1.7rem;font-size:20px;">Jl. Telekomunikasi Terusan Buah Batu Indonesia 40257 , Bandung , Indonesia</p>
+        </div>
+        
+
+      </div>
+    
+      <hr class="clearfix d-md-none rgba-white-light" style="margin: 10% 15% 5%;">
+
+      
+      <div class="row pb-2">
+
+        <div class="col-md-1 col-lg-2 text-center justify-content-center mx-auto my-2" style="display:flex;align-items:center;justify-items:center;">
+          
+            <a>
+              <img src = "<?php echo base_url('application/views/assets/facebook.png'); ?>" style="cursor:pointer;width:60px;margin-right:20px;" href='#'> 
+            </a> 
+            <a >
+               <img src = "<?php echo base_url('application/views/assets/twitter.png'); ?>" style="cursor:pointer;width:60px;margin-right:20px;" href='#'> 
+            </a>  
+            <a >
+              <img src = "<?php echo base_url('application/views/assets/whatsapp.png'); ?>" style="cursor:pointer;width:60px;margin-right:20px;" href='#'> 
+            </a>
+            <a>
+               <img src = "<?php echo base_url('application/views/assets/instagram.png'); ?>" style="cursor:pointer;width:60px;margin-right:20px;" href='#'> 
+            </a>
+
+          
+        </div>
+      </div>
+
+
+            <div class="footer-copyright text-center py-3">© 2019 Copyright, All Rights Reserved
+                <a> GACHA (Magang Seeker)</a>
+            </div>
+
+
+        </footer>
+
+        
+
+        </div>
+        
+    </body>
+</html>
