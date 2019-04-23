0e2e0c45ce563d0ed0a7213c6fe45ffde59da983
geryn25 <35487884+geryn25@users.noreply.github.com>
2019-04-23 21:48:20 +0700

    hampir selese

--- application/views/lowongan/lowongan.php
+++ application/views/lowongan/lowongan.php
@@ -5,11 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+    <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>  
+
  <style>
      
      .Low1{
@@ -43,8 +45,8 @@
  </style>
 </head>
 <body>
-<br><br><br><br>
-    <div class = "container">
+<br><br>
+    <div class = "container-fluid">
             <div class = "row justify-content-md-center">
                     <h3 style = "align-items: center;align-content: center;justify-content: center;display:flex;opacity: 0.8;text-align: center">
                                 Explore GACHA
@@ -55,45 +57,36 @@
                 Temukan Lowongan Pekerjaan yang sesuai dengan kemampuanmu
             </h5>
         </div>
+    <table id="example" class="table table-striped table-bordered" width="100%" height="50px">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Nama Perusahaan</th>
+                        <th>Kategori</th>
+                        <th>Jurusan</th>
+                        <th>Durasi magang</th>
+                        <th>Valid sampai</th>
+                        <th>Semester yang diambil</th>
+                        <th>SKS yang diambil</th>
+                        <th>IPK minimal</th>
+                        <th>PersyaratanUmum</th>
+                        <th>PersyaratanKhusus</th>
+                        <th>Daftar</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php $table=$this->LowonganModel->tampilsemua();
+                        echo $table;
+                    ?>
+                </tbody>
 
-        
-        <div class="row justify-content-md-center">
-                    <div class="col-sm text-center text-md-center p-2 Low1" href="test"><a href="test">
-                      <img src = "<?php echo base_url('application/views/lowongan/assets/Administration.png'); ?>" style="cursor:pointer;" href='#'>
-                        <br>
-                        <h5 style="cursor:pointer;">Administrasi & Koordinasi</h5></a>
-                    </div>
-                    <div class="col-sm text-center text-md-center p-2  Low1">
-                            <img src = "<?php echo base_url('application/views/lowongan/assets/Media.png'); ?>" style="cursor:pointer;" href='#'>
-                            <h5 style="cursor:pointer;"> Media Kreatif</h5>
-                        </div>
-                    <div class="col-sm text-center text-md-center p-2  Low1">
-                            <img src = "<?php echo base_url('application/views/lowongan/assets/Accounting.png'); ?>" style="cursor:pointer;" href='#'>
-                            <h5 style="cursor:pointer;">Akuntansi & Finansial</h5>
-                        </div>
-                    <div class="col-sm text-center text-md-center p-2  Low1">
-                            <img src = "<?php echo base_url('application/views/lowongan/assets/General.png'); ?>" style="cursor:pointer;" href='#'>
-                            <h5 style="cursor:pointer;">General Service</h5>
-                        </div>
-        </div>
-        <div class="row justify-content-md-center">
-                <div class="col-sm text-center text-md-center p-2  Low1">
-                  <img src = "<?php echo base_url('application/views/lowongan/assets/sdm.png'); ?>" style="cursor:pointer;" href='#'>
-                    <h5 style="cursor:pointer;">Sumber Daya Manusia</h5>
-                </div>
-                <div class="col-sm text-center text-md-center p-2  Low1">
-                        <img src = "<?php echo base_url('application/views/lowongan/assets/IT.png'); ?>" style="cursor:pointer;" href='#'>
-                        <h5 style="cursor:pointer;">IT & Software</h5>
-                    </div>
-                <div class="col-sm text-center text-md-center p-2  Low1">
-                        <img src = "<?php echo base_url('application/views/lowongan/assets/Management.png'); ?>" style="cursor:pointer;" href='#'>
-                        <h5 style="cursor:pointer;">Manajemen & Konsultan</h5>
-                    </div>
-                <div class="col-sm text-center text-md-center p-2  Low1">
-                        <img src = "<?php echo base_url('application/views/lowongan/assets/Sales.png'); ?>" style="cursor:pointer;" href='#'>
-                        <h5 style="cursor:pointer;">Sales & Marketing</h5>
-                    </div>
-    </div>
+            </table>   
+    
     </div>
 </body>
+<script>
+    $(document).ready(function() {
+    $('#example').DataTable();
+} );
+</script>
 </html>
